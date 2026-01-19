1a0c5c5720a96feeb6dacb1bf8617551a950f443
Juan Castañeda castro <camiloflstudio@gmail.com>
2026-01-19 16:44:06 -0500

    feat(environments) adding files for pdn and local

---
+++ src/environments/environments.prod.ts
@@ -0,0 +1,18 @@
+export const environment = {
+    //for production
+    production: true,
+    firebase: {
+        apiKey: "AIzaSyB5qI5bEC-qflI7zxv_Hj4Yqn-lLg0wXUY",
+        authDomain: "sgsst-c988a.firebaseapp.com",
+        projectId: "sgsst-c988a",
+        storageBucket: "sgsst-c988a.appspot.com",
+        messagingSenderId: "988833698347",
+        appId: "1:988833698347:web:7820a24db9a6446863906e",
+        measurementId: "G-NHQPFRX950"
+    },
+    authorizedEmails: [
+        'camiloflstudio@gmail.com', 
+        'juan.castaneda@sofka.com.co', 
+        'danielaquintero19@gmail.com'
+    ]
+};
